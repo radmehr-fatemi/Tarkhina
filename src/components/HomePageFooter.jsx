d0c365394e8b57dd34ce550cdcf7ba4d642b15d0
Alireza Fatemi <fa1381138100@gmail.com>
2023-09-26 09:27:18 +0330

    edited the HomePage footer

--- src/components/HomePageFooter.jsx
+++ src/components/HomePageFooter.jsx
@@ -14,49 +14,59 @@ const HomePageFooter = () => {
         <div className={styled.homePageFooterBackground}>
             <div className={styled.homePageFooter} >
 
-                <div className={styled.homePageFooterSection1}>
-                    <h1> دسترسی آسان </h1>
+                <div className={styled.footerField1}>
 
-                    <ul>
-                        <li><Link> پرسش‌های متداول </Link></li>
-                        <li><Link> قوانین ترخینه </Link></li>
-                        <li><Link> حریم خصوصی </Link></li>
-                        <li>
-                            <Link><img src={twiterSVG} alt="twitter photo" /></Link>
-                            <Link><img src={instagramSVG} alt="instagram photo" /></Link>
-                            <Link><img src={telegramSVG} alt="telegram photo" /></Link>
-                        </li>
-                    </ul>
+                    <div className={styled.homePageFooterSection1}>
+                        <h1> دسترسی آسان </h1>
+
+                        <ul>
+                            <li><Link> پرسش‌های متداول </Link></li>
+                            <li><Link> قوانین ترخینه </Link></li>
+                            <li><Link> حریم خصوصی </Link></li>
+                            <li>
+                                <Link><img src={twiterSVG} alt="twitter photo" /></Link>
+                                <Link><img src={instagramSVG} alt="instagram photo" /></Link>
+                                <Link><img src={telegramSVG} alt="telegram photo" /></Link>
+                            </li>
+                        </ul>
+                    </div>
+
+                    <div className={styled.homePageFooterSection2}>
+                        <h1> شعبه‌های ترخینه </h1>
+
+                        <ul>
+                            <li><Link> شعبه اکباتان </Link></li>
+                            <li><Link> شعبه چالوس </Link></li>
+                            <li><Link> شعبه اقدسیه </Link></li>
+                            <li><Link> شعبه ونک </Link></li>
+                        </ul>
+                    </div>
                 </div>
 
-                <div className={styled.homePageFooterSection2}>
-                    <h1> شعبه‌های ترخینه </h1>
+                <div className={styled.footerField2}>
 
-                    <ul>
-                        <li><Link> شعبه اکباتان </Link></li>
-                        <li><Link> شعبه چالوس </Link></li>
-                        <li><Link> شعبه اقدسیه </Link></li>
-                        <li><Link> شعبه ونک </Link></li>
-                    </ul>
-                </div>
+                    <div className={styled.footerField2Details}>
+                        <h1> پیام به ترخینه  </h1>
 
-                <form className={styled.homePageFooterSection3}>
-                    <h1> پیام به ترخینه  </h1>
-
-                    <div className={ styled.homePageFooterSection3Flex }>
                         <div className={styled.footerDetails}>
-                            <input type="text" placeholder='نام و نام خانوادگی' />
-                            <input type="text" placeholder='شماره تماس' />
-                            <input type="email" placeholder='آدرس ایمیل (اختیاری)' />
-                        </div>
+                            <div className={ styled.footerDetailsInputs }>
+                                <input type="text" placeholder='نام و نام خانوادگی' />
+                                <input type="text" placeholder='شماره تماس' />
+                                <input type="email" placeholder='آدرس ایمیل (اختیاری)' />
+                            </div>
 
-                        <div className={styled.footerMassage}>
-                            <input type="text" placeholder='پیام شما' />
+
+                            <div className={styled.footerField2Massage}>
+                                <input type="text" placeholder='پیام شما' />
+                            </div>
                         </div>
                     </div>
 
-                    <button type="submit"> ارسال پیام </button>
-                </form>
+                    <div className={styled.footerField2Button}>
+                        <button type="submit"> ارسال پیام </button>
+                    </div>
+                </div>
+
             </div>
         </div>
     );
