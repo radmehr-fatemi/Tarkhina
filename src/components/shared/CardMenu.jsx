3a56515e8b1073cb29145fa86fa84283f601b48d
Alireza Fatemi <fa1381138100@gmail.com>
2023-09-29 19:41:33 +0330

    edited

--- src/components/shared/CardMenu.jsx
+++ src/components/shared/CardMenu.jsx
@@ -27,27 +27,29 @@ const CardMenu = ({ foodData }) => {
             <div className={styled.cardMenuFields}>
                 <div className={styled.cardMenuField1}>
                     <h4> {name} </h4>
-                    <div className={ styled.cardMenuField1Spans }>
+                    <div className={styled.cardMenuField1Spans}>
                         <span> 220000 </span>
                         <span> % {discount} </span>
                     </div>
                 </div>
 
                 <div className={styled.cardMenuField2}>
-                    <p> { shortHandler( title ) } ... </p>
-                    <span> { (+price).toLocaleString() }  تومان  </span>
+                    <p> {shortHandler(title)} ... </p>
+                    <span> {(+price).toLocaleString()}  تومان  </span>
                 </div>
 
                 <div className={styled.cardMenuField3}>
                     <img src={heartSVG} alt="heart photo" />
-                    <div className={ styled.cardMenuField3Spans }>
+                    <div className={styled.cardMenuField3Spans}>
                         <img src={starSVG} alt="star photo" />
                         <img src={starSVG} alt="star photo" />
                         <img src={starSVG} alt="star photo" />
                         <img src={starSVG} alt="star photo" />
                         <img src={starSVG} alt="star photo" />
                     </div>
-                    <button> افزودن به سبد خرید </button>
+                    <div className={ styled.cardMenuField3Button }>
+                        <button> افزودن به سبد خرید </button>
+                    </div>
                 </div>
             </div>
         </div>
