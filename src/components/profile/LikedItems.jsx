644b3ed0b99b0a651d2f9ebd4fe0f685835cde38
Alireza Fatemi <fa1381138100@gmail.com>
2023-10-02 19:35:48 +0330

    absolute the empty part to LikedItems

--- src/components/profile/LikedItems.jsx
+++ src/components/profile/LikedItems.jsx
@@ -1,5 +1,5 @@
 import React, { useContext } from 'react';
-import { Link ,useNavigate } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 
 //Style
 import styled from "./LikedItems.module.scss";
@@ -10,6 +10,9 @@ import { CartContext } from '../context/CartContextProvider';
 //SVG
 import backSVG from "./svg/arrow-back.svg";
 import trashBlackSVG from "./svg/trashBlack.svg";
+
+//GIF
+import ghostGif from "./gif/ghost.gif"
 
 //Components
 import CardMenu from '../shared/CardMenu';
@@ -23,14 +26,21 @@ const LikedItems = () => {
         <div className={styled.likedItems} >
 
             <div className={styled.likedItemsHeader}>
-                <img src={backSVG} alt="back photo" onClick={ () => navigate( -1 ) } />
+                <img src={backSVG} alt="back photo" onClick={() => navigate(-1)} />
                 <h3> علاقه مندی ها </h3>
-                <img src={trashBlackSVG} alt="trash photo" onClick={ () => dispatch({ type: "CLEAR_LIKEDITEMS" }) } />
+                <img src={trashBlackSVG} alt="trash photo" onClick={() => dispatch({ type: "CLEAR_LIKEDITEMS" })} />
             </div>
 
-            <div className={ styled.likedItemsProducts }>
+            <div className={styled.likedItemsProducts}>
                 {
-                    state.likedItems.map(food => <CardMenu key={food.id} foodData={food} />)
+                    !state.likedItems.length ?
+                        <div className={ styled.likedItemsProductsEmpty } >
+                            <p> شما هنوز غذای مورد علاقه خود را انتخاب نکردید </p>
+                            <img src={ghostGif} alt="ghost photo" />
+                            <Link to="/menu/main" > رفتن به منو </Link>
+                        </div> :
+
+                        state.likedItems.map(food => <CardMenu key={food.id} foodData={food} />)
                 }
             </div>
         </div>
