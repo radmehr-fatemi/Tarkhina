cc05e872a1bc8ed037888f74380d119d4fba7a5c
Alireza Fatemi <fa1381138100@gmail.com>
2023-10-03 10:26:55 +0330

    made the search part in mobile design

--- src/components/HomePageSearch.jsx
+++ src/components/HomePageSearch.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 //Style
 import style from "./HomePageSearch.module.scss";
@@ -6,17 +6,47 @@ import style from "./HomePageSearch.module.scss";
 //SVG
 import searchSVG from "../assets/svg/searchHome.svg";
 
+//Gif
+import searchNonGIF from "../assets/gif/research-non.gif";
+
+//URL
+import { foodsData } from '../data/foodsData';
+
+//Components
+import CardMenu from './shared/CardMenu';
 
 const HomePageSearch = () => {
+
+    const overallData = [...foodsData.iranian, ...foodsData.non_Iranian, ...foodsData.pizzas, ...foodsData.sandwiches];
+    const [input, seInput] = useState("");
+    const [touched, setTouched] = useState(false);
+
+    const changeHandler = ({ target }) => {
+        seInput(target.value)
+        target.value.length ? setTouched(true) : setTouched(false)
+    }
+
+    const searchProducts = overallData.filter(food => food.name.split(" ")[0].includes(input) ?? food.name.includes(input))
+
     return (
         <div>
             <div className={style.homePageSearch}>
-                
+
                 <div className={style.homePageInputSearch} >
-                    <input type="text" placeholder=' جست و جو ' />
+                    <input type="text" placeholder=' جست و جو ' value={input} onChange={changeHandler} />
                     <img src={searchSVG} alt="search photo" />
                 </div>
-                
+
+                <div className={style.homePageInputSearchProducts} style={touched ? { display: "block" } : { display: "none" }} >
+                    {
+                        searchProducts.length ?
+                            searchProducts.map(food => <CardMenu key={food.id} foodData={food} />) :
+                            <div className={ style.homePageInputSearchNon } >
+                                <p> محصول مورد نظر یافت نشد </p>
+                                <img src={ searchNonGIF } alt="search photo" />
+                            </div>
+                    }
+                </div>
             </div>
 
         </div>
